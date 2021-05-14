f936cf9a7691a6fa72f39541b8563bf7fa035e7f
Kuldeep Pandey <kuldeep.msvm@gmail.com>
2021-05-15 00:44:27 +0530

    added CRUD functionalities

--- base/views.py
+++ base/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.contrib.auth.views import LoginView
+
+from django.urls import reverse_lazy
+from base.models import Task
+
 
 # Create your views here.
+
+class MyLoginView(LoginView):
+    template_name = 'base/login.html'
+    field = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'base/tasks.html'
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = "base/task.html"
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    template_name = "base/task-create.html"
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    template_name = "base/task-create.html"
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
+
+    
+
+
+
